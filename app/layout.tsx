c775026a5d2fa32fa2218a21919b328e9c2a7e0c
Marie Marcos <mmdevq@gmail.com>
2025-01-30 11:21:16 -0800

    constants

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,18 @@ import type { Metadata } from "next";
 //font:
 import { Inter } from "next/font/google";
 import '@/assets/styles/globals.css';
+import { APP_DESC, APP_NAME, SERVER_URL } from "@/lib/constants";
 
 const inter = Inter({subsets: ['latin']});
 
+//Use metadata set Ex 'Home | Nextstore' or default
 export const metadata: Metadata = {
-  title: "Next Store",
-  description: "Ecommerce platform built with Next.js",
+  title: {
+    template: `%s | NextStore`,
+    default: APP_NAME,
+  },
+  description: APP_DESC,
+  metadataBase: new URL(SERVER_URL)
 };
 
 export default function RootLayout({
