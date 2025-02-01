3e6411afad674eb68673a4d5a638ae97d507b549
Marie Marcos <mmdevq@gmail.com>
2025-01-31 16:52:22 -0800

    sample data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,15 @@
-// export const metadata = {
-//   title: 'Home',
-// }
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/db/sample-data";
 
 const HomePage = () => {
-  return <>NextStore</>;
-}
+  // console.log(sampleData);
+  return <>
+      <ProductList 
+        data={sampleData.products} 
+        title='Newest Arrivals'
+        // 4 products
+        limit={4} />
+    </>;
+};
  
 export default HomePage;
