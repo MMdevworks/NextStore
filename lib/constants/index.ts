69d6e063f3343e7966129668c0a96825ad1a5f16
Marie Marcos <mmdevq@gmail.com>
2025-02-04 10:20:05 -0800

    seed to db

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,3 +1,4 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NextStore';
 export const APP_DESC = process.env.NEXT_PUBLIC_APP_DESC || 'Next.js Ecommerce';
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
