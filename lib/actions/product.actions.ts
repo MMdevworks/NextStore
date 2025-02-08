1605f01707a2d73271979a48aca4825e0e0484f9
Marie Marcos <mmdevq@gmail.com>
2025-02-08 12:31:42 -0800

    serverless env config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,11 +1,9 @@
 'use server';
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "../utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 //get latest products
 export async function getLatestProducts(){
-    const prisma = new PrismaClient();
-
     const data = await prisma.product.findMany({
         //show limit defined in constants/index.ts
         take: LATEST_PRODUCTS_LIMIT,
